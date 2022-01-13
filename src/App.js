eff3bf0533a35fd2ab6e15005bd7e1da36a1cd6e
jhtdod <jess.dodge@hotmail.co.uk>
2022-01-13 15:52:44 +0000

    filter A-Z work in progress

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import StoryContainer from './containers/StoryContainer';
 
 function App() {
+  
+  const filters = [
+    {name: "title A-Z", value: "title A-Z"},
+    {name: "title Z-A", value: "title Z-A"}
+  ]
+
   return (
     <div className='story-container'>
-      <StoryContainer/>
+      <StoryContainer filters={filters}/>
     </div>
   );
 }
--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import StoryList from '../components/StoryList';
+import TitleBar from '../components/TitleBar';
 
-const StoryContainer = () =>{
+const StoryContainer = ({filters}) =>{
     
     const [stories, setStories] = useState([]);
     const [storyIdArray, setStoryId] = useState([]);
+    const [filteredStories, setFilteredStories] = useState([])
 
     useEffect(() => {
         getStories();
@@ -13,6 +15,10 @@ const StoryContainer = () =>{
     useEffect(() => {
         getStoryId();
     }, [])
+
+    // useEffect(() => {
+    //     filterStories();
+    // }, [])
 
     const getStoryId = function(){
         fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
@@ -29,20 +35,45 @@ const StoryContainer = () =>{
         const promises = urls.map(url => fetch(url).then(resp => resp.json()))
 
         Promise.all(promises).then(stories => setStories(stories))
-
+        setFilteredStories(stories)
         console.log(stories)
         
     }
 
+    const filterStories = value => {
+        let filteredStories;
+        
+        if (value === "title A-Z") {
+             
+            filteredStories = stories.sort(function(a, b) {
+                    const nameA = a.title.toUpperCase();
+                    const nameB = b.title.toUpperCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+                    return 0;
+                })
+            setFilteredStories(filteredStories);
+            
+        } else {
+            setFilteredStories(stories);
+        }
+        
+    } 
+
+    const handleSelectChange = event => {
+        event.preventDefault()
+        filterStories(event.target.value);
+    }
+
     return(
         <>
-            <header>
-                <h1>
-                    Hacker News
-                </h1>
-            </header>
+            <TitleBar handleSelectChange = {handleSelectChange} filters={filters}/>
             <div>
-                <StoryList stories = {stories}/>
+                <StoryList stories = {filteredStories}/>
             </div>
         </>
     )
